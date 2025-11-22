abe55c8095c1323ae3d60b6bf04741571e2a2bf2
Joshua Schneider <pugking4@protonmail.com>
2025-11-22 21:59:40 +1100

    Added to github

---
+++ src/Album.java
@@ -0,0 +1,4 @@
+import java.time.LocalDate;
+import java.util.List;
+
+public record Album(String id, String name, String cover, LocalDate releaseDate, String releaseDatePrecision, String type, List<Artist> artists) {}
