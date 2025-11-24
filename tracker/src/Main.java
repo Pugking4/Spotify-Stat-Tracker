a397d762295e0bfc4733845a3ea6de388f8d2126
Joshua Schneider <pugking4@protonmail.com>
2025-11-24 18:01:52 +1100

    bweh

--- tracker/src/Main.java
+++ tracker/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
         Scheduler scheduler = new Scheduler();
+        // Probably add scheduler.start() or something to keep app running
     }
 }
