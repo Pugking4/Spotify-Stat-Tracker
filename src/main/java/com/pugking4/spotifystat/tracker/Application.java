e787ec63d89e2fa04474649af856aa80c7b14cb8
pugking4 <pugking4@protonmail.com>
2025-12-31 16:10:08 +1100

    fix: removed debug code from Application.java

--- src/main/java/com/pugking4/spotifystat/tracker/Application.java
+++ src/main/java/com/pugking4/spotifystat/tracker/Application.java
@@ -7,11 +7,6 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            SpotifyWrapper.getCurrentlyPlayingTrack();
-        } catch (Exception e) {
-            Logger.println(e);
-        }
         List<ScheduledTask> scheduledTasks = List.of(new TrackingPoller(), new ArtistUpdater());
         Scheduler scheduler = new Scheduler(scheduledTasks);
     }
