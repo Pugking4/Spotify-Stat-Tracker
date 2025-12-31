4f07f09bb54a57133be7ab78dadc75df58c90764
Joshua Schneider <pugking4@protonmail.com>
2025-12-31 16:32:01 +1100

    Delete resources directory

--- resources/.bat
+++
@@ -1,8 +0,0 @@
-sudo docker run -d \
-  --name track-database \
-  -e POSTGRES_USER=pugking4 \
-  -e POSTGRES_PASSWORD=apples \
-  -e POSTGRES_DB=track-database \
-  -p 5433:5432 \
-  -v track-database:/var/lib/postgresql/data \
-  postgres:18
